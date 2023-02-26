9e49e39581caad7e1a8130e6d714e13aa4aa8711
Roman Khilchenko <extrimelife@hotmail.com>
2023-02-26 17:17:51 -0500

    add some MARK in the TabBarViewController and set NAvigation

--- ViewControllers/TabBarViewController.swift
+++ ViewControllers/TabBarViewController.swift
@@ -7,42 +7,48 @@
 
 import UIKit
 
-class TabBarViewController: UITabBarController {
+final class TabBarViewController: UITabBarController {
     
-    let mainViewController = MainViewController()
-    let secondVC = SecondViewController()
+    // MARK: - Private Properties
     
-    private let label: UILabel = {
-        let label = UILabel()
-        label.text = "MeTube"
-        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
-        label.textColor = .white
-        return label
+    private let mainViewController = MainViewController()
+    private let secondVC = SecondViewController()
+    
+    private let navigationLabel: UILabel = {
+        let navigationLabel = UILabel()
+        navigationLabel.text = "MeTube"
+        navigationLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        navigationLabel.textColor = .white
+        return navigationLabel
     }()
     
-    private let imageView: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "MeTube"))
-        imageView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        imageView.contentMode = .scaleAspectFit
-        return imageView
+    private let navigationImageView: UIImageView = {
+        let naviImageView = UIImageView(image: UIImage(named: "MeTube"))
+        naviImageView.frame = CGRect(x: 0, y: 0, width: 10, height: 10)
+        naviImageView.contentMode = .scaleAspectFit
+        return naviImageView
     }()
     
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [imageView, label])
+    private lazy var navigationStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [navigationImageView, navigationLabel])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.spacing = -60
-        stackView.frame.size = CGSize(width: imageView.frame.size.width + label.frame.size.width,
-                                      height: max(imageView.frame.size.height, label.frame.size.height
-                                                 ))
+        stackView.frame.size = CGSize(
+            width: navigationImageView.frame.size.width + navigationLabel.frame.size.width,
+            height: max(navigationImageView.frame.size.height, navigationLabel.frame.size.height))
         return stackView
     }()
+    
+    // MARK: - Override Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
         setupNavigationBar()
     }
+    
+    // MARK: - Private Methods
     
     private func setupNavigation() {
         let mainVC = UINavigationController(rootViewController: mainViewController)
@@ -57,18 +63,12 @@ class TabBarViewController: UITabBarController {
     }
     
     private func setupNavigationBar() {
-    
-        mainViewController.navigationItem.titleView = stackView
         
         let navBarAppearance = UINavigationBarAppearance()
         navBarAppearance.backgroundColor = UIColor(hexString: "#2e0142")
-        
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        
+        mainViewController.navigationItem.titleView = navigationStackView
         mainViewController.navigationController?.navigationBar.standardAppearance = navBarAppearance
         mainViewController.navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-        
         mainViewController.navigationController?.navigationBar.tintColor = .white
     }
 }
