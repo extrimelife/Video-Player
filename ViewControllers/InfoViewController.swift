c0eab8c689591f72e7a9b2fbc914df9044e2e4a7
Roman Khilchenko <extrimelife@hotmail.com>
2023-03-10 21:49:22 -0500

    change color

--- ViewControllers/InfoViewController.swift
+++ ViewControllers/InfoViewController.swift
@@ -11,7 +11,7 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .blue
     }
 }
 
