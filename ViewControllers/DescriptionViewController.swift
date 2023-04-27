e7e9b316701db865fcc7f89055205def68cedd13
Roman Khilchenko <extrimelife@hotmail.com>
2023-04-27 00:22:44 -0400

    added UITableView in DescriptionVC

--- ViewControllers/DescriptionViewController.swift
+++ ViewControllers/DescriptionViewController.swift
@@ -7,10 +7,44 @@
 
 import UIKit
 
-class DescriptionViewController: UIViewController {
-
+final class DescriptionViewController: UIViewController {
+    
+    private lazy var descriptionTableView: UITableView = {
+        let descriptionTableView = UITableView(frame: .zero, style: .insetGrouped)
+        descriptionTableView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTableView.delegate = self
+        descriptionTableView.dataSource = self
+        return descriptionTableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray
+        setupLayout()
     }
+    
+    private func setupLayout() {
+        view.addSubview(descriptionTableView)
+        NSLayoutConstraint.activate([
+            descriptionTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            descriptionTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            descriptionTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            descriptionTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+extension DescriptionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
+
+extension DescriptionViewController: UITableViewDelegate {
+    
 }
