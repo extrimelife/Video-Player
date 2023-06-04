61a881b11d080c543b655c782ec1f8118a7dccc1
Roman Khilchenko <extrimelife@hotmail.com>
2023-06-04 00:42:14 -0400

    add emptyView

--- Components/EmptyView.swift
+++ Components/EmptyView.swift
@@ -7,14 +7,49 @@
 
 import UIKit
 
-class Empty: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class EmptyView: UIView {
+    
+    // MARK: - Private Properties
+    
+    private var totalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private var emptyImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.setContentHuggingPriority(.required, for: .vertical)
+        return imageView
+    }()
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.tintColor = .gray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    // MARK: - Public Methods
+    
+    func show(title: String, image: UIImage) {
+        titleLabel.text = title
+        emptyImage.image = image
+        isHidden = false
     }
-    */
-
+    
+    func hide() {
+        isHidden = true
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupLayout() {
+    }
 }
