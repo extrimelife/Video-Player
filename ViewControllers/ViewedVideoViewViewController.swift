d6483e7b62319d8f2e4ed3d9afd1d0c598675b70
Roman Khilchenko <extrimelife@hotmail.com>
2023-06-12 00:46:08 -0400

    started implement viewedVC

--- ViewControllers/ViewedVideoViewViewController.swift
+++ ViewControllers/ViewedVideoViewViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewedVideoViewViewController: UIViewController {
     
+    private let emptyView = EmptyView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        showEmptyView()
+    }
+    
+    private func showEmptyView() {
+        emptyView.show(title: "No videos viewed yet",
+                       image: UIImage(named: "notFavorite") ?? UIImage())
+        layoutEmptyView()
+    }
+    
+    private func layoutEmptyView() {
+        emptyView.frame = view.bounds
+        view.addSubview(emptyView)
     }
 }
-
